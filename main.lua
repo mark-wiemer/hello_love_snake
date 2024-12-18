f068b43f32e8e82ad09b6841e3417b1b3e6b9a72
Mark Wiemer <markwiemer@outlook.com>
2024-12-17 18:31:15 -0600

    Add non-moving snake

--- main.lua
+++ main.lua
@@ -1,14 +1,25 @@
 function love.draw()
-    local gridXCount = 20
-    local gridYCount = 15
-    local cellSize = 15
+	local gridXCount = 20
+	local gridYCount = 15
+	local cellSize = 15
 
-    love.graphics.setColor(0.28, 0.28, 0.28)
-    love.graphics.rectangle(
-        'fill',
-        0,
-        0,
-        gridXCount * cellSize,
-        gridYCount * cellSize
-    )
+	love.graphics.setColor(0.28, 0.28, 0.28)
+	love.graphics.rectangle("fill", 0, 0, gridXCount * cellSize, gridYCount * cellSize)
+
+	local snakeSegments = {
+		{ x = 3, y = 1 },
+		{ x = 2, y = 1 },
+		{ x = 1, y = 1 },
+	}
+
+	for segmentIndex, segment in ipairs(snakeSegments) do
+		love.graphics.setColor(0.6, 1, 0.32)
+		love.graphics.rectangle(
+			"fill",
+			(segment.x - 1) * cellSize,
+			(segment.y - 1) * cellSize,
+			cellSize - 1,
+			cellSize - 1
+		)
+	end
 end
