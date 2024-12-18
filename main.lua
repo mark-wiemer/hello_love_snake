09c4666ef30d432bb53094cb2bf2c34b52087790
Mark Wiemer <markwiemer@outlook.com>
2024-12-17 18:53:16 -0600

    Switch to sumneko.lua and cleanup

--- main.lua
+++ main.lua
@@ -1,38 +1,40 @@
+---@diagnostic disable: lowercase-global
 function love.draw()
-	local gridXCount = 20
-	local gridYCount = 15
-	local cellSize = 15
+    local gridXCount = 20
+    local gridYCount = 15
+    local cellSize = 15
 
-	love.graphics.setColor(0.28, 0.28, 0.28)
-	love.graphics.rectangle("fill", 0, 0, gridXCount * cellSize, gridYCount * cellSize)
+    love.graphics.setColor(0.28, 0.28, 0.28)
+    love.graphics.rectangle("fill", 0, 0, gridXCount * cellSize, gridYCount * cellSize)
 
-	local snakeSegments = {
-		{ x = 3, y = 1 },
-		{ x = 2, y = 1 },
-		{ x = 1, y = 1 },
-	}
-
-	for segmentIndex, segment in ipairs(snakeSegments) do
-		love.graphics.setColor(0.6, 1, 0.32)
-		love.graphics.rectangle(
-			"fill",
-			(segment.x - 1) * cellSize,
-			(segment.y - 1) * cellSize,
-			cellSize - 1,
-			cellSize - 1
-		)
-	end
+    for segmentIndex, segment in ipairs(snakeSegments) do
+        love.graphics.setColor(0.6, 1, 0.32)
+        love.graphics.rectangle(
+            "fill",
+            (segment.x - 1) * cellSize,
+            (segment.y - 1) * cellSize,
+            cellSize - 1,
+            cellSize - 1
+        )
+    end
 end
 
 function love.load()
-	-- global
-	timer = 0
+    -- global
+    timer = 0
+    snakeSegments = {
+        { x = 3, y = 1 },
+        { x = 2, y = 1 },
+        { x = 1, y = 1 },
+    }
 end
 
 function love.update(dt)
-	timer = timer + dt
-	if timer >= 0.15 then
-		timer = timer - 0.15
-		print("tick")
-	end
+    timer = timer + dt
+    if timer >= 0.15 then
+        timer = timer - 0.15
+        local nextXPosition = snakeSegments[1].x + 1
+        local nextYPosition = snakeSegments[1].y
+        print("tick")
+    end
 end
